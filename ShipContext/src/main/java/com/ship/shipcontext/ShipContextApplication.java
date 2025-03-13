44f37558d231ae39462bb050cee8997d510a495a
mesutozdemir <mesutz775@gmail.com>
2025-03-13 13:26:22 +0100

    first conversion

---
+++ ShipContext/src/main/java/com/ship/shipcontext/ShipContextApplication.java
@@ -0,0 +1,13 @@
+package com.ship.shipcontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipContextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipContextApplication.class, args);
+    }
+
+}
