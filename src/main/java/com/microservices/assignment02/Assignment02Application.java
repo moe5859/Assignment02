7914ec9a202c100f3af67adf0bf851a3294e1266
mesutozdemir <mesutz775@gmail.com>
2025-03-13 12:26:29 +0100

    initial commit

---
+++ src/main/java/com/microservices/assignment02/Assignment02Application.java
@@ -0,0 +1,13 @@
+package com.microservices.assignment02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment02Application.class, args);
+    }
+
+}
